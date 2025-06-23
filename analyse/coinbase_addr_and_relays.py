57f36b3d52c3d65674e42785a2aad61765e4e639
Nils Beyer <nils.beyer@student.kit.edu>
2025-06-23 15:23:57 +0200

    Update coinbase_addr_and_relays.py to include 'real' scatter

--- analyse/coinbase_addr_and_relays.py
+++ analyse/coinbase_addr_and_relays.py
@@ -45,3 +45,29 @@ ax.axhline(y=0.089, color='blue', linestyle='--', linewidth=1)
 ax.axvline(x=cdf_normalized[cdf_normalized >= 0.089].index[0], color='blue', linestyle='--', linewidth=1)
 ax.set_xscale('log')
 fig.savefig("out/coinbase_addr_and_relays-scatter.png")
+
+# graph of validator addresses
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(40,10), sharey=True, width_ratios=[0.95,0.05])
+
+coinbase_addr_translated = df['coinbase_addr'].apply(int, base=16)
+fraction_relay = df['relay_block_count'] / df['block_count']
+
+ax[0].scatter(coinbase_addr_translated.values, df['block_count'].values, c=fraction_relay, cmap=green_red_cmap)
+ax[0].set_xticks([], [])
+ax[0].set_yscale('log')
+ax[0].set_xlabel("Coinbase Address")
+ax[0].set_ylabel("Number of Blocks")
+
+grouped = df.groupby('block_count')
+grouped_all_sum = grouped['block_count'].sum().sort_index()
+grouped_relay_sum = grouped['relay_block_count'].sum().sort_index()
+
+fraction_relay = grouped_relay_sum / grouped_all_sum
+
+ax[1].scatter([0 for i in range(len(grouped))], grouped["block_count"].first().values, c=fraction_relay, cmap=green_red_cmap)
+ax[1].set_xticks([], [])
+ax[1].set_xlabel("Combined")
+
+fig.tight_layout()
+fig.savefig("out/coinbase_addr_and_relays-real-scatter.png")
