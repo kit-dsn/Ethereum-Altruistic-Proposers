b2287dfef4925648b4b8fce6d743ed9c63c0ecbb
Nils Beyer <nils.beyer@student.kit.edu>
2025-07-20 20:05:59 +0200

    generate a better cluster json

---
+++ analyse/proposer_clusters_merged.py
@@ -0,0 +1,29 @@
+# depends_on: proposer_clusters.py,proposer_collaboration.py
+import pandas as pd
+import json
+from itertools import chain
+
+# get clusters
+with open("out/proposer_clusters-non-relaying-clusters.json") as file:
+    non_relaying_clusters = json.load(file)
+
+# get associated proposers
+non_relaying_proposers = pd.read_json("out/proposer_collaboration-no-relaying-proposer-coinbase.json")
+
+non_relaying_clusters_proposer = []
+for idx, c in enumerate(non_relaying_clusters):
+    p = set()
+    for ca in c:
+        p.update(list(non_relaying_proposers[non_relaying_proposers['coinbase_addr'] == ca]['proposer_index']))
+    
+    non_relaying_clusters_proposer.append(list(p))
+
+# make sure that no proposer appears double
+assert len(list(chain(*(non_relaying_clusters_proposer)))) == len(non_relaying_proposers['proposer_index'].unique())
+assert len(non_relaying_clusters_proposer) == len(non_relaying_clusters)
+
+with open("out/proposer_clusters_merged.json", 'w') as file:
+    file.write(json.dumps({
+        'clusters': non_relaying_clusters,
+        'proposers': non_relaying_clusters_proposer
+    }))
