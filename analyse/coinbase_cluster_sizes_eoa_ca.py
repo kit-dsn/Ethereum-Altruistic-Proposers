bb2307da25e6dad8abf1c0ed900713c2e0eba775
Nils Beyer <nils.beyer@student.kit.edu>
2025-08-11 17:59:05 +0200

    coinbase_cluster_sizes_eoa_ca: add contract clusters by Patrick

--- analyse/coinbase_cluster_sizes_eoa_ca.py
+++ analyse/coinbase_cluster_sizes_eoa_ca.py
@@ -3,6 +3,7 @@ import numpy as np
 from sqlalchemy import create_engine
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
+import json
 import utils.query
 
 # first analyze EOA coinbase addresses
@@ -100,3 +101,61 @@ ax2.fill_between(
     zorder=0  # ensures it stays behind scatter
 )
 fig.savefig("out/coinbase_cluster_sizes_eao_ca-ca-sizes.png")
+
+with open("analyse/coinbase_cluster_sizes_eoa_ca-contract-cluster.json") as file:
+    contract_clusters = json.load(file)
+
+df_ca_cluster = []
+for ccl in contract_clusters:
+    df_c = df_ca[df_ca.index.isin(ccl)]
+    assert len(df_c) == len(ccl)
+
+    df_ca_cluster.append({
+        "coinbase_addr": ccl,
+        "validator_count": df_c['validator_count'].sum(),
+        "block_count": df_c['block_count'].sum(),
+        "relay_block_count": df_c['relay_block_count'].sum()
+    })
+
+df_cac = pd.DataFrame(df_ca_cluster)
+df_cac = df_cac.sort_values(by='validator_count')
+assert df_cac['block_count'].sum() == df_ca['block_count'].sum()
+assert df_cac['validator_count'].sum() == df_ca['validator_count'].sum()
+assert df_cac['relay_block_count'].sum() == df_ca['relay_block_count'].sum()
+
+df_cac.to_csv('/tmp/cac.csv')
+
+groupby_cluster_size_cac = df_cac.groupby('validator_count')
+cluster_sizes_cac = groupby_cluster_size_cac['coinbase_addr'].count()
+cluster_relay_fraction_cac = groupby_cluster_size_cac['relay_block_count'].sum() / groupby_cluster_size_cac['block_count'].sum()
+cluster_block_number_cdf_cac = groupby_cluster_size_cac['block_count'].sum().cumsum()
+cluster_block_number_cdf_cac = cluster_block_number_cdf_cac / cluster_block_number_cdf_cac.iloc[-1]
+
+print(cluster_sizes_cac)
+
+# render cac sizes
+y = cluster_sizes_cac.values
+x = cluster_sizes_cac.index
+
+print(x,y)
+
+fig, ax = plt.subplots(1, 1)
+ax2 = ax.twinx()
+blue_red_cmap = LinearSegmentedColormap.from_list('BlueRed', ['blue', 'red'])
+ax.scatter(x, y, c=cluster_relay_fraction_cac, cmap=blue_red_cmap)
+ax.set_yscale('log')
+ax.set_xscale('log')
+ax.set_xlabel('Number of validators in a coinbase-contract-cluster')
+ax.set_ylabel('Number of coinbase addresses')
+ax.set_xlim(0.5, ax.get_xlim()[1])
+ax2.set_ylim(0, 1)  # linear scale between 0 and 1
+ax2.set_ylabel('Share of blocks')
+ax2.fill_between(
+    np.append(np.repeat(x, 2)[1:],ax.get_xlim()[1]),
+    0,  # start from bottom
+    np.append(np.repeat(cluster_block_number_cdf_cac.values, 2)[:-1], 1),
+    color='orange',
+    alpha=0.2,
+    zorder=0  # ensures it stays behind scatter
+)
+fig.savefig("out/coinbase_cluster_sizes_eao_ca-cac-sizes.png")
