14334f104099d2e32b92cc2929ff8cc1bf66d34e
Nils Beyer <nils.beyer@student.kit.edu>
2025-08-14 17:43:38 +0200

    fix dependencies again?

--- analyse/meta-overview.py
+++ analyse/meta-overview.py
@@ -1,4 +1,4 @@
-#depends_on:coinbase_clutsers.py,non_mev_coinbase_clusters_eoa_ca.py,proposer_collaboration.py
+# depends_on: coinbase_clutsers.py,non_mev_coinbase_clusters_eoa_ca.py,proposer_collaboration.py
 import pandas as pd
 from itertools import chain
 import json
